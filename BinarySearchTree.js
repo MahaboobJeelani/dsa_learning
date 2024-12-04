96733c39d042b0cd2b8ef62e62da90ec9860279e
mahaboob <mahaboob@murphi.ai>
2024-12-04 11:41:19 +0530

    DSA Learning

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,19 +1,69 @@
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value
         this.right = null
         this.left = null
     }
 }
 
-class BinarySearchTree{
-    constructor(){
+class BinarySearchTree {
+    constructor() {
         this.root = null
     }
-    isEmpty(){
+    isEmpty() {
         return this.root === null
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        // this code is for insert the left value which means lesser then the parent node 
+        if (newNode.value < root.value) {
+            if (root.left === null) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left, newNode)
+            }
+        }
+        // this code is for insert the right value which means greater then the parent node 
+        else {
+            if (root.right === null) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
+    search(root, value) {
+        if (!root) {
+            return false
+        } else {
+            if (root.value === value) {
+                return true
+            } else if (value < root.value) {
+                return this.search(root.left, value)
+            } else {
+                return this.search(root.right, value)
+            }
+        }
     }
 }
 
 const bst = new BinarySearchTree();
-console.log(bst.isEmpty());
+bst.insert(10)
+bst.insert(20)
+bst.insert(5)
+// console.log(bst.isEmpty());
+
+console.log(bst.search(bst.root, 60));
+
+
+
